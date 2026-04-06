70d061729232d35adae9682a7a06ac783aa63346
salvadorpulido-commits <salvador.pulido@iesnervion.es>
2026-04-06 10:52:17 +0200

    calculadora

---
+++ src/entornos/Calculadora.java
@@ -0,0 +1,35 @@
+package entornos;
+
+public class Calculadora {
+
+    private int num1;
+    private int num2;
+
+    public Calculadora() {
+    }
+
+    public Calculadora(int a, int b) {
+        num1 = a;
+        num2 = b;
+    }
+
+    public int suma() {
+        int resul = num1 + num2;
+        return resul;
+    }
+
+    public int resta() {
+        int resul = num1 - num2;
+        return resul;
+    }
+
+    public int multiplica() {
+        int resul = num1 * num2;
+        return resul;
+    }
+
+    public int divide() {
+        int resul = num1 / num2;
+        return resul;
+    }
+}
